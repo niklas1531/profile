d458dc458a38bebba8ec7cbd85f51c6368a4daeb
niklas1531 <97844057+niklas1531@users.noreply.github.com>
2022-05-31 17:44:14 +0200

    Add files via upload
    index.js added for dark theme

---
+++ index.js
@@ -0,0 +1,15 @@
+$("#darkMode").click(function () {
+
+    $("#darkMode").text("Light Mode");
+    $(".colored-section").css("backgroundColor", "#303030");
+    $(".white-section").css("backgroundColor", "black");
+    $("p").css("color", "white");
+    $(".section-heading").css("color", "white");
+    $("h3").css("color", "white");
+    $(".skills-image").css("color", "white");
+    $("a").css("color", "white");
+    $(".btn-success").css("backgroundColor", "black");
+    $(".btn-success").css("borderColor", "white");
+
+
+});
